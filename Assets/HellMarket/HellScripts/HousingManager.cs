4e14f30c3f8d51dc4e36bcd2be986ada5bf2a47f
TurtleWaters <TurtleWaters@users.noreply.github.com>
2023-02-05 17:34:49 -0800

    housing wipe and reorganization of assets

---
+++ Assets/HellMarket/HellScripts/HousingManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HousingManager 
+{
+    //there once was a boy from tennessee
+        //he lived so merry and full of glee
+        //until his dog died.
+
+
+
+
+
+
+}
